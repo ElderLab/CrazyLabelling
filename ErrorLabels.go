4da6262f1f68e19d2d494ef6467844ad10c01e5c
tot0p <thomas.lemaitre.78420@gmail.com>
2025-03-05 14:23:15 +0100

    add some errors labels

--- ErrorLabels.go
+++ ErrorLabels.go
@@ -4,6 +4,18 @@ const (
 	// ErrInvalidBody : Bad Request with invalid Body
 	ErrInvalidBody = "Invalid Body"
 
+	// ErrInvalidQuery : Bad Request with invalid Query
+	ErrInvalidQuery = "Invalid Query"
+
+	// ErrInvalidUrlParam : Bad Request with invalid URL Param
+	ErrInvalidUrlParam = "Invalid URL Param"
+
 	// ErrInternalServer : Internal Server Error
 	ErrInternalServer = "Internal Server Error"
+
+	// ErrInvalidToken : Invalid Token
+	ErrInvalidToken = "Invalid Token"
+
+	// ErrInvalidCredentials : Invalid Credentials
+	ErrInvalidCredentials = "Invalid Credentials"
 )
