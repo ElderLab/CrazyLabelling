4c31a9e03d484d48076962a367bd9a2ddfd6a0a7
tot0p <thomas.lemaitre.78420@gmail.com>
2025-04-16 16:43:57 +0200

    add Lanes list

---
+++ Team/Member/Lanes.go
@@ -0,0 +1,14 @@
+package Member
+
+var lanes = []string{
+	"Top",
+	"Jungle",
+	"Mid",
+	"Bot",
+	"Support",
+}
+
+// Lanes returns the list of lanes in League of Legends.
+func Lanes() []string {
+	return lanes
+}
